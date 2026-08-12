9b43aea2e413c5a38edc289a6dd08fe6c4a22e9c
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2026-08-12 22:43:44 +0800

    feat: add withoutPushEntitlement plugin to remove push notification entitlement

---
+++ plugins/withoutPushEntitlement.js
@@ -0,0 +1,8 @@
+const { withEntitlementsPlist } = require("expo/config-plugins");
+
+module.exports = function withoutPushEntitlement(config) {
+  return withEntitlementsPlist(config, (config) => {
+    delete config.modResults["aps-environment"];
+    return config;
+  });
+};
